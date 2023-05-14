8a3c7ea752aed9ff694a56ffdb3feb0699aac559
Mohamed_Yasser <mohamed.yasser99999@gmail.com>
2023-05-14 21:35:34 +0300

    Solving Contest_B from codeforces

---
+++ CodeForces/Contest_B/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        Integer a = x.nextInt();
+        Long b = x.nextLong();
+        String c = x.next();
+        Float d = x.nextFloat();
+        Double e = x.nextDouble();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
