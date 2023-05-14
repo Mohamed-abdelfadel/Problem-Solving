a9430acf4e1e0150c9418f7809ded4cd3d5309a8
Mohamed_Yasser <mohamed.yasser99999@gmail.com>
2023-05-14 21:37:51 +0300

    Solved Contest_A from CodeForces

---
+++ CodeForces/Contest_A/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        String input = x.next();
+        System.out.println("Hello, " + input);
+    }
+}
